5dc10c4d71932e771f9c9240b3f935eabb8008a2
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-07 23:30:24 -0700

    get usd rate

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,21 @@
 
 module Main where
 
-import Network.HTTP.Simple ( httpBS, getResponseBody )
-import qualified Data.ByteString.Char8 as BS
+import           Network.HTTP.Simple            ( httpBS, getResponseBody )
+import           Control.Lens                   ( preview )
+import           Data.Aeson.Lens                ( key, _String )
+import qualified Data.ByteString.Char8         as BS
+import           Data.Text                      ( Text )
 
 fetchJSON :: IO BS.ByteString
 fetchJSON = do
   res <- httpBS "https://api.coindesk.com/v1/bpi/currentprice.json"
   return (getResponseBody res)
 
+getRate :: BS.ByteString -> Maybe Text
+getRate = preview (key "bpi" . key "USD" . key "rate" . _String)
+
 main :: IO ()
 main = do
   json <- fetchJSON
-  BS.putStrLn json
+  print (getRate json)
